267709d46e0cef23ea82b631a6e40738cd0f719c
GilesAlonso <86449189+GilesAlonso@users.noreply.github.com>
2023-06-20 15:11:59 +0000

    updated files

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,20 +1,24 @@
 import Chart from 'chart.js/auto'
+import { getAquisitionsByYear } from './api'
 
 (async function() {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+  const data = await getAquisitionsByYear();
 
   new Chart(
     document.getElementById('acquisitions'),
     {
       type: 'bar',
+      options: {
+        animation: true,
+        plugins: {
+          legend: {
+            display: true
+          },
+          tooltip: {
+            enabled: true
+          }
+        }
+      },
       data: {
         labels: data.map(row => row.year),
         datasets: [
