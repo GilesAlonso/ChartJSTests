162a4056a38675df1c65c3f5ed8255e0c8d1c656
GilesAlonso <86449189+GilesAlonso@users.noreply.github.com>
2023-06-22 19:47:19 +0000

    testing import data

--- src/apiRows.js
+++ src/apiRows.js
@@ -1,6 +1,6 @@
 var url = 'https://api.rows.com/v1beta1/spreadsheets/3Fc6yUcb0iZ8UjMUKR4IdA/tables/0d460a7f-6cc1-4e6a-a857-97f7b20c8bf4/values/A1:H';
 
-var apiKey = process.env.{ROWS22062023};
+var apiKey = process.env.ROWS22062023;
 
 var headers = {
   'Authorization': 'Bearer ' + apiKey
@@ -20,6 +20,10 @@ var data = JSON.parse(response.getContentText());
 // var values = data.map(function(item) {
 //   return item.value;
 // });
+var output = document.getElementById('testData');
+function getElementById(output) {
+    output.innerHtml = data
+}
 
 
 console.log(data)
